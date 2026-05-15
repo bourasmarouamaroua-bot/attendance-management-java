1cc596bfd804b55acbd8c2bedf1731e43d0ac380
lamokoko <lyliaboubchir30@gmail.com>
2026-05-15 22:41:05 +0100

    lamotpushpush

---
+++ src/exceptions/ExcessiveAbsenceException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ExcessiveAbsenceException extends Exception {
+
+    public ExcessiveAbsenceException(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/InvalidLoginException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class InvalidLoginException extends Exception {
+
+    public InvalidLoginException(String message) {
+        super(message);
+    }
+}
+
